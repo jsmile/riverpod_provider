3526a4d00242658570b5decb888c8187134a535b
jsmile <jsmile@naver.com>
2023-12-10 20:14:28 +0900

    riverpod Provider & ConsumerWidget

--- lib/src/pages/basic/basic_page.dart
+++ lib/src/pages/basic/basic_page.dart
@@ -2,26 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_provider/src/pages/basic/basic_provider.dart';
 
-class BasicPage extends StatelessWidget {
+class BasicPage extends ConsumerWidget {
   const BasicPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final hello = ref.watch(helloProvider);
+    final world = ref.watch(worldProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Provider'),
       ),
       body: Center(
-        child: Consumer(
-          builder: (context, ref, child) {
-            final hello = ref.watch(helloProvider);
-            final world = ref.watch(worldProvider);
-
-            return Text(
-              '$hello $world',
-              style: Theme.of(context).textTheme.headlineLarge,
-            );
-          },
+        child: Text(
+          '$hello $world',
+          style: Theme.of(context).textTheme.headlineLarge,
         ),
       ),
     );
