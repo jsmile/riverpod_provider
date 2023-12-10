1df63dc5851f1c423f7624454c39ab6bb57b7656
jsmile <jsmile@naver.com>
2023-12-10 22:38:11 +0900

    riverpod Provider.autoDispose.famile<T, Object>() & Equatable

--- lib/src/pages/auto_dispose_family/auto_dispose_family_provider.dart
+++ lib/src/pages/auto_dispose_family/auto_dispose_family_provider.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final autoDisposeFamilyHelloProvider =
@@ -9,4 +10,29 @@ final autoDisposeFamilyHelloProvider =
   });
 
   return 'Hello $name';
+});
+
+class Counter extends Equatable {
+  final int count;
+
+  const Counter({
+    required this.count,
+  });
+
+  @override
+  String toString() => 'Counter(count: $count)';
+
+  @override
+  List<Object> get props => [count];
+}
+
+final counterProvider =
+    Provider.autoDispose.family<int, Counter>((ref, counter) {
+  print('### counterProvider $counter : created !');
+
+  ref.onDispose(() {
+    print('### counterProvider : disposed !');
+  });
+
+  return counter.count;
 });
